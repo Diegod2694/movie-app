b031cc34ff3da1540a4793df07dd4df4135636f9
Diego Parra <Diegod2694@gmail.com>
2019-06-15 13:51:53 -0500

    basic master-detail layout

---
+++ src/components/MoviesList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { selectMovie } from '../actions/selectMovie'
+
+const MoviesList = ( { movies, selectMovie } ) => {
+  const listItems = movies.map( movie => {
+    return (
+      <div key={movie.title}>
+        <span>{movie.title}</span>
+        <button onClick={() => selectMovie( movie )} >Detalles</button>
+      </div>
+    )
+  } )
+
+  return (
+    <div className='MoviesList-container'>
+      <h2>Lista de Peliculas</h2>
+      <ul>
+        {listItems}
+      </ul>
+    </div>
+  )
+}
+
+const mapStateToProps = ({movies}) => {
+  return {
+    movies,
+  }
+}
+
+const mapDispatchToProps = {
+  selectMovie,
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( MoviesList )
